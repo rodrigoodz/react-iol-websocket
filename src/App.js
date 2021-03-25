21da65107365dacd47b595c084a6f53efa11103c
Rodrigo Ortiz de Zarate <rodrigoodz_6@hotmail.com>
2021-03-24 22:33:20 -0300

    eliminado styled de App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from "styled-components";
 import Websocket from "./components/Websocket";
 import { Helmet } from "react-helmet";
 import Title from "./components/Title";
